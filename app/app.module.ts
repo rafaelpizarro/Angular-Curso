ce21e875023d35f8d3d81acc3c89805d264d8ab9
Rafael Pizarro <rpbahamondez@gmail.com>
2021-03-24 10:50:15 -0300

    primer commit

---
+++ app/app.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+//import { ContadorComponent } from './contador/contador/contador.component';
+import { HeroeModule } from './heroes/heroes.module';
+import { ContadorModule } from './contador/contador.module';
+
+
+@NgModule({
+  declarations: [
+    AppComponent//,
+    //ContadorComponent
+  ],
+  imports: [
+    BrowserModule,
+    HeroeModule,
+    ContadorModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
